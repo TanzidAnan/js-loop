b38149a2eacf4c32d9abfda5234f1cbe6c7d27fd
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-02 23:26:50 +0600

    event sun

---
+++ different.js
@@ -0,0 +1,27 @@
+for(let i = 0; i<=20; i++){
+    if(i%2 ===0){
+        // console.log(i)
+    }
+}
+
+
+
+for(let i = 0; i<=20; i++){
+    if(i%2 !==0){
+        // console.log(i);
+    }
+}
+
+for(let i =1; i<20 ; i+=2){
+    // console.log(i)
+}
+
+
+let totle =1;
+for(let i = 0; i<=20; i++){
+    if(i%3 ===0){
+        console.log(i);
+        totle =totle+i;
+    }
+}
+console.log("Total Number ",totle)
