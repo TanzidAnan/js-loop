b0bed668ecac03f9020ee450e11d1e6ff333c0b0
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-02 14:30:54 +0600

    while loop

--- while.js
+++ while.js
@@ -3,3 +3,11 @@ const number = 7;
 if(number < 10){
     console.log('soto akta number paisi')
 }
+
+let num = 0;
+while(num <5){
+    console.log('loooooooooping',num);
+    // num =num+1;
+    // num +=1;
+    num ++;
+}
