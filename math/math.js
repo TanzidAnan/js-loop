2e762fc2afbf3b66949c6d23143a63b3884259a7
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-15 00:22:19 +0600

    math

---
+++ math/math.js
@@ -0,0 +1,14 @@
+/**
+ * Number: +ve, -ve, 0  [25,-8]
+ * 2. integer:2 54 23 [odd ,even]
+ * 3,float:52.4,54.6 ,87.5
+ * 4.operation: +, -, *, /, 
+ * 5.more operation:%,
+ * 6.percentage:5%
+ * 7.sum: 14, 82, 54
+ * 8.avg:14, 82, 54
+ * 9.max/min
+ * 10.series:
+ * 11.area
+ * 12.
+ * */ 
